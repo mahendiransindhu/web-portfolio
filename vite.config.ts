893eb531feb38e491134eb4b71a35a8bd63cec57
Mahendiran Sindhu <25006266@studentmail.ul.ie>
2025-10-24 23:01:11 +0100

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@
   import path from 'path';
 
   export default defineConfig({
-    plugins: [react()],
+  base: "/web-portfolio/",
+  plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
